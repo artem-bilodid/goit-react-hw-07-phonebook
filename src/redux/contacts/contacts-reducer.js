069bd0542519ac5e07f220347ed6eb139a6b6dec
Artem Bilodid <ar.bilodid@draftkings.com>
2021-12-21 22:08:34 +0200

    removed extra loading handlers

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -4,34 +4,18 @@ import {
   getContactsRequest,
   getContactsSuccess,
   getContactsError,
-  addContactRequest,
   addContactSuccess,
-  addContactError,
-  deleteContactRequest,
   deleteContactSuccess,
-  deleteContactError,
 } from './contacts-actions';
 
-const items = createReducer(
-  [],
-  {
-    [getContactsSuccess]: (state, { payload }) => payload,
-  },
-  // [getContactsSuccess]: (_, { payload }) => payload,
-  // [deleteContactSuccess]: (state, { payload }) =>
-  //   state.filter(contact => contact.id !== payload.id),
-);
+const items = createReducer([], {
+  [getContactsSuccess]: (state, { payload }) => payload,
+});
 
 const isLoading = createReducer(true, {
   [getContactsRequest]: () => true,
   [getContactsSuccess]: () => false,
   [getContactsError]: () => false,
-  [addContactRequest]: () => true,
-  [addContactSuccess]: () => false,
-  [addContactError]: () => false,
-  [deleteContactRequest]: () => true,
-  [deleteContactSuccess]: () => false,
-  [deleteContactError]: () => false,
 });
 
 const isInvalidated = createReducer(true, {
